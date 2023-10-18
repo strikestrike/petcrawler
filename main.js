b2c84ecffcc14cfd80703d91e683eb14ccef6137
DESKTOP-9DAE3QG\Administrator <newssstrike@gmail.com>
2023-10-19 01:25:32 +0300

    super/fix: scheduling enabled

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const scraper = require('./scraper/scraper');
 const scheduler = require('./scheduler/scheduler');
 
 // Start the scheduler to automate scraping
-// scheduler.startScheduler();
+scheduler.startScheduler();
 
 // Optionally, you can run the scraper manually as well
 scraper.startScraping();
