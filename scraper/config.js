f5fe9b015b62fd2147ac4db68640eb7e293c2ac1
strikestrike <newssstrike@gmail.com>
2023-10-25 09:29:48 +0000

    super/fix: config scrapping url modified for page

--- scraper/config.js
+++ scraper/config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	searchUrls: [
-		// 'https://www.petfinder.com/search/dogs-for-adoption/ca/newfoundland-and-labrador/?distance=Anywhere&sort%5B0%5D=available_longest',
 		'https://www.petfinder.com/search/dogs-for-adoption/ca/newfoundland-and-labrador/?distance=Anywhere&page=468&sort%5B0%5D=available_longest',
 		//'https://www.petfinder.com/search/cats-for-adoption/ca/newfoundland-and-labrador/?distance=Anywhere&sort%5B0%5D=available_longest',
 	],
